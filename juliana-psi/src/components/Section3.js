a3a10ad678643a65a4b45bf40c665651b12c393d
Lucas Marques <lucas.marks8@gmail.com>
2023-04-29 21:34:43 -0300

    acerto de alguns erros

--- juliana-psi/src/components/Section3.js
+++ juliana-psi/src/components/Section3.js
@@ -3,14 +3,14 @@ import React from 'react'
 
 const Section3 = () => {
     return (
-        <div id='Section3' className='flex flex-col gap-y-2 h:fit bg-gradient-to-r from-emerald-100  via-fuchsia-300 to-pink-100  max-[940px]:h-fit'>
+        <div id='Section3' className='flex flex-col gap-y-2 h:fit bg-gradient-to-r from-emerald-100  via-fuchsia-300 to-pink-100 max-[940px]:h-fit'>
             <div className='flex justify-center mt-10 mb-8'>
                 <div className='flex items-center w-[20rem] justify-center bg-gradient-to-r from-emerald-100  via-fuchsia-300 to-pink-100 rounded-full text-gray-700'>
                     ÁREAS DE ATUAÇÃO
                 </div>
             </div>
             <div className='flex flex-col items-center justify-center h-fit gap-2 max-[940px]:h-fit'>
-                <div className='flex items-center justify-center h-fit gap-2 max-[940px]:grid max-[940px]:grid-cols-1 max-[940px]:h-fit max-[940px]:mt-6'>
+                <div className='flex items-center justify-center h-fit gap-2 max-[940px]:grid max-[940px]:grid-cols-1 max-[940px]:h-fit max-[940px]:mt-4'>
                     <div className='flex flex-col text-center items-center justify-around h-72 w-72 p-1 bg-white rounded-lg hover:bg-gradient-to-r from-emerald-100  via-fuchsia-300 to-pink-100 hover:border-brown-50 hover:border-4'>
                         <div className='flex justify-center h-48 w-48 rounded-full'>
                             <img src='/images/terapia-individual.jpeg' width={"300px"} height={"300px"} className='rounded-full hover:border-brown-50 hover:border-4' />
